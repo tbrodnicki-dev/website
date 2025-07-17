06626718b7ced9898698b1deb86a17f6287dd839
Tyler <serpbtw@gmail.com>
2025-07-16 22:24:48 -0400

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+import Particles from './Particles';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  const items = [
+  ];
+  
+
+  return (
+    <>
+    <div style={{ width: '100vw', height: '100vh', position: 'fixed' }}>
+      <Particles
+        particleColors={['#ffffff', '#ffffff']}
+        particleCount={1000}
+        particleSpread={30}
+        speed={0.2}
+        particleBaseSize={100}
+        moveParticlesOnHover={false}
+        alphaParticles={true}
+        disableRotation={false}
+      />
+    </div>
+    <div className='main-content'>
+      <h1>Hi, my name is Tyler!</h1>
+      <p>I'm a Third-Year Computer Science student at the University of Florida! At my time at UF I've been apart of many organizations that have allowed me to freely practice my skills in Software Engineering!</p>
+    </div>
+    </>
+  )
+}
+
+export default App
