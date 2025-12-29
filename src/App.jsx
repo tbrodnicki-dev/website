d8930e050f91ae4b5b5926d3c6b0d1aa29513362
TBrodnicki <brodnicki.tj@gmail.com>
2025-12-28 23:12:20 -0500

    Included Dummy Photos for Representation

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Lightbox  from 'yet-another-react-lightbox';
 import 'yet-another-react-lightbox/styles.css';
 import './index.css'
 import Particles from './Particles';
+import { Counter, Fullscreen, Inline, Share, Zoom } from 'yet-another-react-lightbox/plugins';
 
 function ParticlesShowcase() {
   return (
@@ -24,11 +25,20 @@ function ParticlesShowcase() {
 }
 
 function App() {
-  const [count, setCount] = useState(0);
   const [index, setIndex] = useState(-1);
   const photos = [
-  { src: "/test1.jpg", width: 800, height: 600 },
-  { src: "/test2.jpg", width: 1000, height: 1333 },
+  { src: "/1.jpeg", width: 1280, height: 853 },
+  { src: "/2.jpeg", width: 1280, height: 853 },
+  { src: "/3.jpeg", width: 853, height: 1280 },
+  { src: "/4.jpeg", width: 1280, height: 853 },
+  { src: "/5.jpeg", width: 853, height: 1280 },
+  { src: "/6.jpeg", width: 1280, height: 853 },
+  { src: "/7.jpeg", width: 853, height: 1280 },
+  { src: "/8.jpeg", width: 853, height: 1280 },
+  { src: "/9.jpeg", width: 853, height: 1280 },
+  { src: "/10.jpeg", width: 853, height: 1280 },
+  { src: "/11.jpeg", width: 853, height: 1280 },
+  { src: "/12.jpeg", width: 1280, height: 853 },   
 ];
   
 
@@ -40,25 +50,32 @@ function App() {
       <h2>Portfolio</h2>
     </div>
     <RowsPhotoAlbum 
-    photos={photos}
-    spacing={10}
-    padding={10}
-    rowConstraints={{ singleRowMaxHeight: 350 }}
-    onClick={({ index }) => setIndex(index)} />
+      photos={photos}
+      spacing={10}
+      padding={10}
+      rowConstraints={{ singleRowMaxHeight: 350 }}
+      onClick={({ index }) => setIndex(index)} />
     <Lightbox
         slides={[
-  { src: "/test1.jpg"},
-  { src: "/test2.jpg"},
-]}
+          { src: "/1.jpeg"},
+          { src: "/2.jpeg"},
+          { src: "/3.jpeg"},
+          { src: "/4.jpeg"},
+          { src: "/5.jpeg"},
+          { src: "/6.jpeg"},
+          { src: "/7.jpeg"},
+          { src: "/8.jpeg"},
+          { src: "/9.jpeg"},
+          { src: "/10.jpeg"},
+          { src: "/11.jpeg"},
+          { src: "/12.jpeg"},
+        ]}
         open={index >= 0}
         index={index}
         close={() => setIndex(-1)}
+        plugins={[Fullscreen, Zoom, Share]}
       />
     </>
-    
-
-
-
   )
 }
 
